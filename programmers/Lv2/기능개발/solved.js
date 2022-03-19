24dc157f5f44bd93fc6fe94c100346de9a5c3b8e
kimmsoll <kimmsoll@naver.com>
2022-03-19 21:29:25 +0900

    1 solved programmers

--- programmers/Lv2/기능개발/solved.js
+++ programmers/Lv2/기능개발/solved.js
@@ -1,3 +1,32 @@
+// 두 번째 풀이
+function solution(progresses, speeds) {
+    let needDays = [];
+    let answer = [];
+    
+    if (progresses.length === 1) return [1];
+    
+    for(let i=0; i<progresses.length; i++) {
+        needDays.push(Math.ceil((100 - progresses[i])/speeds[i]));
+    }
+    needDays.reverse();
+
+    let queue = [];
+    
+    while(needDays.length > 0) {
+        let cnt = 1;
+        queue.push(needDays.pop());
+        while (queue[0] >= needDays[needDays.length - 1]) {
+            cnt += 1;
+            needDays.pop();
+        }
+        answer.push(cnt);
+        queue = [];
+        
+    }
+    return answer;
+}
+
+// 첫 번째 풀이
 function solution(progress, speed) {
     // Math.ceil(n) 은 n보다 크거나 같은 정수 중 가장 작은 정수를 반환한다
     let arr = progress.map((v, idx) => Math.ceil((100-v)/speed[idx]));
