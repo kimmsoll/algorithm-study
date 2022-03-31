5e143f03672e8686df1857e13bf4d6b766f6a7b3
kimmsoll <kimmsoll@naver.com>
2022-03-31 23:03:45 +0900

    3 solved hackerRank

---
+++ hackerRank/gradingStudents.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+    // Write your code here
+    let answer = grades.map((v) => {
+        if ((v % 10 === 3 || v % 10 === 8) && v + 2 >= 40) {
+            return v + 2;
+        } else if ((v % 10 === 4 || v % 10 === 9) && v + 1 >= 40) {
+            return v + 1;
+        } else return v;
+    });
+    return answer;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
+ 
